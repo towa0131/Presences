b1bf7a30431df25f20b961657b238616cd45f3d4
towa0131 <towa.towa0131@gmail.com>
2021-02-21 15:09:42 +0900

    Added Seiyanoheya presence

---
+++ websites/S/Seiyanoheya/presence.ts
@@ -0,0 +1,24 @@
+const presence = new Presence({
+    clientId: "812909969657298945"
+});
+
+var browsingStamp = Math.floor(Date.now() / 1000);
+
+presence.on("UpdateData", async () => {
+    if (document.location.pathname.startsWith("/game")) {
+        const title = document.title.replace("誠也の部屋【", "").replace(" 攻略】", "");
+
+        const presenceData: PresenceData = {
+            details: `${title} を攻略中`,
+            largeImageKey: "logo",
+            startTimestamp: browsingStamp
+        };
+
+        if (presenceData.details == null) {
+            presence.setTrayTitle()
+            presence.setActivity();
+        } else {
+            presence.setActivity(presenceData);
+        }
+   }
+});
