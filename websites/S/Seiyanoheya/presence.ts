28e78905186146d751b82782b6e5fd2ca6106dd2
towa0131 <towa.towa0131@gmail.com>
2021-02-21 21:40:46 +0900

    Update presence.ts

--- websites/S/Seiyanoheya/presence.ts
+++ websites/S/Seiyanoheya/presence.ts
@@ -1,24 +1,24 @@
 const presence = new Presence({
-    clientId: "812909969657298945"
+  clientId: "812909969657298945"
 });
 
 var browsingStamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
-    if (document.location.pathname.startsWith("/game")) {
-        const title = document.title.replace("誠也の部屋【", "").replace(" 攻略】", "");
+  if (document.location.pathname.startsWith("/game")) {
+    const title = document.title.replace("誠也の部屋【", "").replace(" 攻略】", "");
 
-        const presenceData: PresenceData = {
-            details: `${title} を攻略中`,
-            largeImageKey: "logo",
-            startTimestamp: browsingStamp
-        };
+    const presenceData: PresenceData = {
+      details: `${title} を攻略中`,
+      largeImageKey: "logo",
+      startTimestamp: browsingStamp
+    };
 
-        if (presenceData.details == null) {
-            presence.setTrayTitle()
-            presence.setActivity();
-        } else {
-            presence.setActivity(presenceData);
-        }
-   }
+    if (presenceData.details == null) {
+      presence.setTrayTitle()
+      presence.setActivity();
+    } else {
+      presence.setActivity(presenceData);
+    }
+  }
 });
